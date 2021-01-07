013ec2e63567f90533d178a6272d3764c06a057d
anastasiak <anastasiya.koryakina1@gmail.com>
2021-01-07 19:48:47 +0100

    changed all fonts, created styles for Library

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -5,13 +5,13 @@ const Library = ({ songs }) => {
   return(
     <div className="library">
       <h2>Library</h2>
-      <div className="library-song">
+      <div>
         {songs.map((song) => (
           <LibrarySong song={song}/>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Library;
