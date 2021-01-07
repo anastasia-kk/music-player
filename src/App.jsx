a9674a07d0bd3defae3e94cff4764ec5a65f494d
anastasiak <anastasiya.koryakina1@gmail.com>
2021-01-07 17:46:22 +0100

    Library and LibrarySong components added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./styles/app.scss";
 //Components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 import { chillHop as data } from "./data";
 
 
@@ -21,6 +22,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs}/>
     </div>
   );
 }
